12f5a8aeb254c6b449c4d6e0e354d363ba2a8659
Marcos Lima <marcosbew2@hotmail.com>
2021-09-16 18:34:08 -0300

    Create pacotes.bat

---
+++ pacotes.bat
@@ -0,0 +1,18 @@
+echo -------------------------
+echo Pacotes
+echo -------------------------
+
+echo "Pacote para automatizacao"
+pip install pyautogui
+
+echo "Pacote para falar"
+pip install pyttsx3
+
+echo "Pacote para conversão de .py para .exe"
+pip install pyinstaller
+
+echo "Pacote para postar no instagram"
+pip install instabot
+
+echo "Pacote para baixar a imagem do perfil do instagram"
+pip install instaloader
