a2749890d673147ddea27f9faa9e21ccd71c78c4
Marcos Lima <marcosbew2@hotmail.com>
2022-02-23 09:26:09 -0300

    Ajustes

---
+++ pacotes de atualização.bat
@@ -0,0 +1,30 @@
+echo -------------------------
+echo Pacotes - Atualizar
+echo -------------------------
+
+echo "Atualizar o PIP"
+pip install --upgrade pip
+
+echo "Pacote para Image"
+pip install --upgrade Image
+
+echo "Pacote para falar"
+pip install --upgrade pyttsx3
+
+echo "Pacote para automatizacao"
+pip install --upgrade pyautogui
+
+echo "Pacote para conversão de .py para .exe"
+pip install --upgrade pyinstall --upgrade er
+
+echo "Pacote PIL"
+pip install --upgrade Pillow
+
+echo "Pacote de calculo"
+pip install --upgrade tkinter-math
+
+echo "Pacote de OpenCV"
+pip install --upgrade opencv-python
+
+echo "Pacote de Cliboard"
+pip install --upgrade pyperclip
---
+++ pacotes de instalação.bat
@@ -0,0 +1,31 @@
+echo -------------------------
+echo Pacotes - Instalar
+echo -------------------------
+
+echo "Instalar o PIP"
+pip install pip
+
+echo "Pacote para Image"
+pip install Image
+
+echo "Pacote para falar"
+pip install pyttsx3
+
+echo "Pacote para automatizacao"
+pip install pyautogui
+
+echo "Pacote para conversão de .py para .exe"
+pip install pyinstall 
+
+echo "Pacote PIL"
+pip install Pillow
+
+echo "Pacote de calculo"
+pip install tkinter-math
+
+echo "Pacote de OpenCV"
+pip install opencv-python
+
+echo "Pacote de Cliboard"
+pip install pyperclip
+
